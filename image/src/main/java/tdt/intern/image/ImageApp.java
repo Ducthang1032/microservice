4f7db64e80bc0de8c320963a5c4c0ab806f87a7c
Ducthang1032 <72100889+Ducthang1032@users.noreply.github.com>
2024-02-02 11:54:37 +0700

    Add files via upload

---
+++ image/src/main/java/tdt/intern/image/ImageApp.java
@@ -0,0 +1,16 @@
+package tdt.intern.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ImageApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApp.class, args);
+    }
+
+}
+
